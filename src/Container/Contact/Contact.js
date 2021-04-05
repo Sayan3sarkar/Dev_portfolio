ce9e800f923aa017361391243db0c0e26c970aef
sayan_sarkar@pursuitsoftware.biz-at-888391330587 <sayansarkar333@gmail.com>
2021-04-05 12:47:36 +0530

    Deployed on 5/4/2021

--- src/Container/Contact/Contact.js
+++ src/Container/Contact/Contact.js
@@ -15,7 +15,7 @@ const Contact = () => {
                 <div className="contact_map">
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3683.6683555869904!2d88.39777031545667!3d22.59150398517312!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a02760a5789f559%3A0x5373355076886790!2sP.N.B.!5e0!3m2!1sen!2sin!4v1617474173521!5m2!1sen!2sin"
-                        width="380"
+                        width="350"
                         height="450"
                         style={{ border: '1rem solid #191d2b', borderRadius: '1rem' }}
                         allowFullScreen=""
