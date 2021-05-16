891f7096bdb4836c29924890c45caafe7d4d2611
sayan_sarkar@pursuitsoftware.biz-at-888391330587 <sayansarkar333@gmail.com>
2021-05-16 11:25:06 +0530

    Updated Blogs

--- src/shared/blogs.js
+++ src/shared/blogs.js
@@ -1,11 +1,18 @@
 import blog_1 from '../img/blogs/blog_1.png';
 import blog_2 from '../img/blogs/blog_2.png';
 import blog_3 from '../img/blogs/blog_3.png';
+import blog_4 from '../img/blogs/blog_4.png';
 export const blogs = [
     {
         blogName: 'Advanced JavaScript: Working of JavaScript Engine',
         blogURL: 'https://sayansarkar333.medium.com/advance-javascript-working-of-javascript-engine-3c36c1a46f50',
         blogImage: blog_3
+
+    },
+    {
+        blogName: 'React: Behind the scenes',
+        blogURL: 'https://sayansarkar333.medium.com/react-behind-the-scenes-c3613ffb9130',
+        blogImage: blog_4
 
     },
     {
